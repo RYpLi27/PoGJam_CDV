d459057a581301cee1ed8b9a4310968ca0024e52
Zbigniew "RYpLi" Janasek <zjanasek@edu.cdv.pl>
2025-02-08 00:24:13 +0100

    Camera action

--- PoGJam_CDV/Assets/Scripts/PlayerLook.cs
+++ PoGJam_CDV/Assets/Scripts/PlayerLook.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+    private void Start()
     {
-        
+        transform.Rotate(1, 1, 0);
     }
-
-    // Update is called once per frame
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        
+        float horizontal = horizontalSpeed * Input.GetAxis("Mouse X");
+        float vertical = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(-vertical, horizontal, 0);
     }
 }
--- PoGJam_CDV/Assets/Scripts/PlayerMovement.cs
+++ PoGJam_CDV/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 10.0f;
     [SerializeField] float rotationSpeed = 1.0f;
+    [SerializeField] GameObject player;
+    [SerializeField] float turnSpeed;
+    [SerializeField] float horizontalInput;
+    [SerializeField] float forwardInput;
     // Update is called once per frame
     void FixedUpdate()
     {
+        //float yMouse = Input.GetAxis("Mouse X");
+        //float xMouse = Input.GetAxis("Mouse Y");
 
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.position = player.transform.position;
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Rotate(Vector3.right, turnSpeed * horizontalInput * Time.deltaTime);
+
     }
+
+                                                          //Best wishes, -Lincoln Green 
 }
